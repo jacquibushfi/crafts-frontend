222f332dd8b3eb7d0b4307a691a301245a387466
jacquibushfi <info@stainedglassofmckinney.com>
2020-03-10 13:14:01 -0500

    changing things in the craftscontainer and app.js so app.js can be handle router, cleaning up files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { fetchCrafts } from './actions/fetchCrafts'
 import CraftsContainer from './containers/CraftsContainer'
 
 
 class App extends React.Component {
 
 
+  componentDidMount() {
+    this.props.fetchCrafts()
+  }
+
   render() {
+    
     return (
       <div className="App">
-        <CraftsContainer />
+       <CraftsContainer crafts={this.props.crafts}/>
       </div>
     );
   }
 }
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    crafts: state.craftReducer.crafts,
+    loading: state.craftReducer.loading
+  }
+}
+export default connect(mapStateToProps, {fetchCrafts})(App);
--- src/containers/craftsContainer.js
+++ src/containers/craftsContainer.js
@@ -1,41 +1,23 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { fetchCrafts } from '../actions/fetchCrafts'
-import Crafts from '../components/Crafts'
 import CraftInput from '../components/CraftInput'
-
-
 class CraftsContainer extends React.Component {
 
-  componentDidMount() {
-    this.props.fetchCrafts()
-  }
-
-  handleLoading = () => {
-    if (this.props.loading) {
-      return <h3>Loading...</h3>
-    } else {
-      return <Crafts crafts={this.props.crafts} />
-    }
-  }
+  
 
   render() {
+    const craftsList = this.props.crafts.map((craft, i) => <li key={i}>{craft.name}</li>)
+
     return (
       <div>
-        <h3>Create Craft</h3>
+        <h3>CRAFTS</h3>
         <CraftInput />
-        <h3>Crafts</h3>
-        {this.handleLoading()}
+        <hr></hr>
+       <ul>
+          {this.props.loading ? <h3>Loading...</h3> : craftsList}
+        </ul>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    crafts: state.craftReducer.crafts,
-    loading: state.craftReducer.loading
-  }
-}
-
-export default connect(mapStateToProps, { fetchCrafts })(CraftsContainer)
+export default CraftsContainer
