99bc0320970389888905f4fb4e2cf533684f6002
jacquibushfi <info@stainedglassofmckinney.com>
2020-03-10 15:58:11 -0500

    verifying menus and displays

--- src/containers/craftsContainer.js
+++ src/containers/craftsContainer.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+
 class CraftsContainer extends React.Component {
 
   
 
   render() {
-    const craftsList = this.props.crafts.map((craft, i) => <li key={i}>{craft.name}</li>)
+    const craftsList = this.props.crafts.map((craft, i) =>
+     <li key={i}>
+       <Link to={`/crafts/${craft.id}`}>{craft.name}</Link>
+     </li>)
 
     return (
       <div>
