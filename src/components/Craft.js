5485f7bd181e172248e5a75a2436f17842d53429
jacquibushfi <info@stainedglassofmckinney.com>
2020-03-10 09:54:06 -0500

    creating crafts.js to setup drilling down into supplies per craft

---
+++ src/components/Craft.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Craft = (props) => {
+
+  return (
+    <div>
+      {props.craft.name}
+    </div>
+  )
+
+
+}
+
+export default Craft
--- src/components/Crafts.js
+++ src/components/Crafts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Craft from './Craft'
 
 
 class Crafts extends React.Component {
