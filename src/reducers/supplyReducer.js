2c50531528f2022e4ffca7d64607548698e388c2
jacquibushfi <info@stainedglassofmckinney.com>
2020-03-12 20:55:34 -0500

    cleaning up

--- src/reducers/supplyReducer.js
+++ src/reducers/supplyReducer.js
@@ -1,18 +1,5 @@
 export default (state = { crafts: [], loading: false }, action) => {
   switch (action.type) {
-
-    // case "ADD_SUPPLY":
-    //   return {
-    //     ...state,
-    //     loading: true
-    //   }
-
-    //   case "SUPPLY_ADDED":
-    //     return {
-    //     ...state,
-    //     supplies: [...state.supplies, action.payload],
-    //     loading: false
-    //     }
    
     default:
       return state
